d17cea7445d71fa65d106cf058b453f4a24f42c6
Hemendu Roy <hroy6@asu.edu>
2023-03-27 14:39:30 -0700

    Updates

---
+++ precache-manifest.ba7034a0b4e284ce51c457642112c5ea.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e3b770df6b1f4268de2",
+    "url": "/Portoflio/static/css/main.f88b870a.chunk.css"
+  },
+  {
+    "revision": "1e3b770df6b1f4268de2",
+    "url": "/Portoflio/static/js/main.3f9ee331.chunk.js"
+  },
+  {
+    "revision": "5e9a2e413d0ccf720625",
+    "url": "/Portoflio/static/js/runtime~main.fc11f220.js"
+  },
+  {
+    "revision": "59cbbcbd3e6a10ea86ef",
+    "url": "/Portoflio/static/css/2.08d07b7f.chunk.css"
+  },
+  {
+    "revision": "59cbbcbd3e6a10ea86ef",
+    "url": "/Portoflio/static/js/2.d487ef1c.chunk.js"
+  },
+  {
+    "revision": "dea35c407ff4707fbea3",
+    "url": "/Portoflio/static/js/3.916d2a49.chunk.js"
+  },
+  {
+    "revision": "ceb236e3d32fb7e98dca",
+    "url": "/Portoflio/static/js/4.d66c5849.chunk.js"
+  },
+  {
+    "revision": "be1c56a5a9fd923b4ef813d93afb5dc0",
+    "url": "/Portoflio/static/media/resume.be1c56a5.pdf"
+  },
+  {
+    "revision": "27f9ac5ae8912f3a74bd4e02ebcab924",
+    "url": "/Portoflio/static/media/home-main.27f9ac5a.svg"
+  },
+  {
+    "revision": "a2fc495bd1eb4322c1a3014782ee6eca",
+    "url": "/Portoflio/static/media/avatar.a2fc495b.jpg"
+  },
+  {
+    "revision": "a0c5ace087961161dab5c23445d2dc07",
+    "url": "/Portoflio/static/media/about.a0c5ace0.png"
+  },
+  {
+    "revision": "4e74d3b4c4cf786890ea1a63bbcd2414",
+    "url": "/Portoflio/static/media/Smart_Home_Gesture_Recognition.4e74d3b4.png"
+  },
+  {
+    "revision": "0a6de98669dea543776b5e1a7f6903e4",
+    "url": "/Portoflio/static/media/BrainNet.0a6de986.png"
+  },
+  {
+    "revision": "7f24a6dea76255a3d80808251504e6b6",
+    "url": "/Portoflio/static/media/Facial_Recognition.7f24a6de.png"
+  },
+  {
+    "revision": "0914d4e9c456253db72e44f4bf6e64e5",
+    "url": "/Portoflio/static/media/pre.0914d4e9.svg"
+  },
+  {
+    "revision": "01918cadf50e0a2dd4f961841d2d2b5c",
+    "url": "/Portoflio/static/media/home-bg.01918cad.jpg"
+  },
+  {
+    "revision": "79fc87392ebcd2156d6799fb1efa9ea1",
+    "url": "/Portoflio/index.html"
+  }
+];
