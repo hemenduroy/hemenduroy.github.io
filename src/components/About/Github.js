f37ac5153666c711176eb35abb50eaad0647b98f
Hemendu Roy <hroy6@asu.edu>
2023-03-27 15:02:30 -0700

    removing dead title

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -14,16 +14,7 @@ function Github() {
   };
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        When I Have Used <strong className="red">GitHub</strong>
-      </h1>
-      <GitHubCalendar
-        username="hemenduroy"
-        blockSize={15}
-        blockMargin={5}
-        theme={colourTheme}
-        fontSize={16}
-      />
+      
     </Row>
   );
 }
