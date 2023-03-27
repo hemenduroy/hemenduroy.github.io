e80e101c70caea49f7fb0c012da61c83d2ba02f7
Hemendu Roy <hroy6@asu.edu>
2023-03-27 15:09:40 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef86dc10d70133acb81223b19a929c78.js"
+  "/precache-manifest.39b9f154a479fbce6dedce9d432b3774.js"
 );
 
 workbox.clientsClaim();
