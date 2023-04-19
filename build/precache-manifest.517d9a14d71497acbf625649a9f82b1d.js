404b65eaf3bebc8ea427d112bcf132a8609d601b
Hemendu Roy <hroy6@asu.edu>
2023-04-19 10:49:56 -0700

    update build for gh-pages

---
+++ build/precache-manifest.517d9a14d71497acbf625649a9f82b1d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "2500089726cd287be5c5",
+    "url": "/static/css/main.4705acad.chunk.css"
+  },
+  {
+    "revision": "2500089726cd287be5c5",
+    "url": "/static/js/main.595bf768.chunk.js"
+  },
+  {
+    "revision": "f6b76ef32852aa16b6b7",
+    "url": "/static/js/runtime~main.649c74d1.js"
+  },
+  {
+    "revision": "ac4365db743b6d6d269a",
+    "url": "/static/css/2.08d07b7f.chunk.css"
+  },
+  {
+    "revision": "ac4365db743b6d6d269a",
+    "url": "/static/js/2.c11b71dd.chunk.js"
+  },
+  {
+    "revision": "dea35c407ff4707fbea3",
+    "url": "/static/js/3.916d2a49.chunk.js"
+  },
+  {
+    "revision": "ceb236e3d32fb7e98dca",
+    "url": "/static/js/4.d66c5849.chunk.js"
+  },
+  {
+    "revision": "6ec29e2ec4d144aad16ce023210e156f",
+    "url": "/static/media/resume.6ec29e2e.pdf"
+  },
+  {
+    "revision": "27f9ac5ae8912f3a74bd4e02ebcab924",
+    "url": "/static/media/home-main.27f9ac5a.svg"
+  },
+  {
+    "revision": "a2fc495bd1eb4322c1a3014782ee6eca",
+    "url": "/static/media/avatar.a2fc495b.jpg"
+  },
+  {
+    "revision": "a0c5ace087961161dab5c23445d2dc07",
+    "url": "/static/media/about.a0c5ace0.png"
+  },
+  {
+    "revision": "4e74d3b4c4cf786890ea1a63bbcd2414",
+    "url": "/static/media/Smart_Home_Gesture_Recognition.4e74d3b4.png"
+  },
+  {
+    "revision": "0a6de98669dea543776b5e1a7f6903e4",
+    "url": "/static/media/BrainNet.0a6de986.png"
+  },
+  {
+    "revision": "7f24a6dea76255a3d80808251504e6b6",
+    "url": "/static/media/Facial_Recognition.7f24a6de.png"
+  },
+  {
+    "revision": "0914d4e9c456253db72e44f4bf6e64e5",
+    "url": "/static/media/pre.0914d4e9.svg"
+  },
+  {
+    "revision": "01918cadf50e0a2dd4f961841d2d2b5c",
+    "url": "/static/media/home-bg.01918cad.jpg"
+  },
+  {
+    "revision": "50d39baa6a6b54f8a5c2ed1d668a681f",
+    "url": "/index.html"
+  }
+];
